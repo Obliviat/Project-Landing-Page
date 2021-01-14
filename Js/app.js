11f668bc06b8fad183745decf945a8ac57296245
Joanna <joanna.gil09@gmail.com>
2021-01-14 18:27:49 -0500

    completed

--- Js/app.js
+++ Js/app.js
@@ -1,18 +1,19 @@
 //NAVEGATION
 //Navigation is built dynamically as an unordered list.
 
-const navUl = document.getElementById('navegation');
+const navUl = document.getElementById('navegation_ul');
 const SectionsOfPage = document.querySelectorAll('.section');
 const sections = document.getElementsByTagName('section');
 
 
-for (let i = 0; i <= SectionsOfPage.length; i++) {
+for (let i = 0; i < SectionsOfPage.length; i++) {
   const section = SectionsOfPage[i];
   const li = document.createElement('li');
   const anchor = document.createElement('a');
   // with "dataset": represent the data attribute of sections title.
-  // This takes the atribute content data-nav. 
-  const sectionTitle = section.dataset.title ?? '';
+  // This takes the atribute content data-name. 
+  const title = section.dataset.name ?? '';
+
   // I created "id" for each "section" then "href" is created for each link <a>
   const sectionId = section.id;
 
@@ -23,37 +24,33 @@ for (let i = 0; i <= SectionsOfPage.length; i++) {
   // added to <li></li> > <a></a>
   li.appendChild(anchor);
   //  to <li> text of <a>
-  li.querySelector('a').innerText = sectionTitle;
+  li.querySelector('a').innerText = title;
   navUl.appendChild(li);
 }
 
-
 // Scroll to Anchor
 // When clicking an item from the navigation menu, the link should scroll to the appropriate section. 
+//get the title of "sections" 
+const titleOfSection = document.querySelector('.title_tag');
 
-const titleSection = document.querySelectorAll('.title');
-
-function viewport(e) {
-  const rect = e.getBoundingClientReact();
+function isElementInViewpor(el) {
+  const rect = el.getBoundingClientRect();
   return (
-
     rect.bottom >= 0 &&
     rect.right >= 0 &&
     // element is in viewport, its top and left are always greater than or equal to zero.
-    rect.left <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.top <= (window.innerWidth || document.documentElement.clientWidth)
-
+    rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+    rect.top < (window.innerHeight || document.documentElement.clientHeight)
   );
-
 };
+
 
 //set section as a active
 // added "class" active to each section (tag(h2)) when is near of viewport
 function sectionActive() {
-
   for (let i of sections) {
     const id = i.id;
-    if (viewpor(i) == true) {
+    if (isElementInViewpor(i) == true) {
       i.classList.add('active');
       document.querySelector(`[href="#${id}"]`) ?.classList.add('active')
     }
@@ -62,17 +59,16 @@ function sectionActive() {
       document.querySelector(`[href="#${id}"]`) ?.classList.remove('active')
     }
   }
+
 }
 
-
-
 // call the sectionActive which is is on viewport
-window.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', function () {
   sectionActive();
 });
 
 // scroll to top on click of the icon, it uses a smooth effect 
-const scrollUp = document.querySelector('.go-up');
+const scrollUp = document.querySelector('.go_up');
 scrollUp.addEventListener('click', function (e) {
   e.preventDefault();
   window.scrollTo({ left: 0, top: 0, behavior: "smooth" })
